9bbf2ef8af95e97a44ea47dbd6c7eeeb482169c4
suri38 <37613675+suri38@users.noreply.github.com>
2018-03-21 19:36:22 +1100

    Main.tf

---
+++ Main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+        access_key = "AKIAJPVPL4IDSI6QK74A"
+        secret_key = "cjoG9E053qQa69nnsBDeTkn1atWt3nd3d23SDpXM/"
+        region = "us-east-1d"
+}
+
+resource "aws_instance" "SKY01" {
+        ami = "ami-b374d5a5"
+        instance_type = "t2.micro"
+}
+	tags {
+    	Name = "SKY01"
+  }
+  $ terraform init
+  $ terraform apply
