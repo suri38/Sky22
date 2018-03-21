28b89e824ff4434a0946df464ecc9ce549977d37
suri38 <37613675+suri38@users.noreply.github.com>
2018-03-21 20:04:28 +1100

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,15 +1,17 @@
+{
 provider "aws" {
         access_key = "AKIAIOBWVSLOXKG6RNBQ"
         secret_key = "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"
         region = "us-east-1d"
-}
+        }
 
 resource "aws_instance" "instance_Ex" {
         ami = "ami-b374d5a5"
         instance_type = "t2.micro"
 
-tags {
-    	Name = "SKY01"
-     }
-}
+        tags {
+        Name = "SKY01"
+             }
+         }
 run terraform apply
+}
