3c3827bd0276883c95060167d60eb5cd69204e50
suri38 <37613675+suri38@users.noreply.github.com>
2018-03-27 23:28:47 +1100

    Main.tf

--- Main.tf
+++ Main.tf
@@ -5,8 +5,48 @@ provider "aws" {
         }
 
 
-resource "aws_elb" "sky" {
-        name="sky_elb"
+ 
+resource "aws_launch_configuration" "sky_ASG" {
+        ami = "ami-408c7f28"
+        instance_type = "t2.micro"
+        security_groups = ["${aws_security_group.instance.id}"]
+            
+        lifecycle{
+                create_before_destroy = true
+                }
+        }       
+resource "aws_security_group" "new_insta"{
+        name = "sky_sec_insta"
+        
+        ingress{
+               from_port = "${var.server_port}"
+                to_port = "${var.server_port}"
+                protocol= "tcp"
+                cidr_blocks = ["0.0.0.0/0] 
+                }
+        
+        lifecycle{
+                create_before_destroy = true
+                }
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "sky_ASG"{
+launch_configuration = "${aws_launch_configuration.sky_ASG.id}"
+min_size = 2
+max_size = 4
+
+tag{
+key="name"
+value ="new_asg"
+propagate_at_launch = true
+}
+}
+
+
+resource "aws_elb" "sky_elb" {
+        name="new_asg"
         availability_zones = ["${data.aws_availability_zones.all.names}"]
         instances = ["${aws_instance.sky.*.id}"]
         listener{
@@ -16,10 +56,3 @@ resource "aws_elb" "sky" {
                instance_protocol="http"
               }
         }
-
- 
-resource "aws_instance" "sky" {
-        count = "4"
-        ami = "ami-408c7f28"
-        instance_type = "t2.micro"
-             }       
